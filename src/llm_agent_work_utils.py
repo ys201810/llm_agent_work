b0f4aedb2cb2efc09b49aaec6e7a5d8b474f9ad0
shirai1 <shirai1@macnoMacBook-Pro.local>
2024-03-30 23:40:56 +0900

    [mofidied] 対象年と期間や金額を抽出するプロンプトの追加

--- src/llm_agent_work_utils.py
+++ src/llm_agent_work_utils.py
@@ -1,10 +1,12 @@
 # coding=utf-8
+import pathlib
+
 import yaml
 from collections import namedtuple
 from openai import OpenAI
 
 
-def get_config(config_file: str) -> (str, str):
+def get_config(config_file: str, base_path: pathlib.Path) -> (str, str, pathlib.Path):
     """
     APIKEYを取得する。
     :param config_file(str): APIKEYが記載されているyamlファイルのパス
@@ -13,6 +15,7 @@ def get_config(config_file: str) -> (str, str):
     Config = namedtuple('Config',[
         'openai_api_key',
         'openai_model',
+        'base_path',
     ])
 
     with open(config_file, 'r') as inf:
@@ -22,22 +25,31 @@ def get_config(config_file: str) -> (str, str):
     config = Config(
         openai_api_key=openai_api_key,
         openai_model=openai_model,
+        base_path=base_path
     )
 
     return config
 
 
-def call_llm(prompt: str, config: namedtuple):
+def call_llm(prompt: str, config: namedtuple, context: str = None, is_calc_final: bool = False):
     client = OpenAI(
         # This is the default and can be omitted
         api_key=config.openai_api_key,
     )
 
+    if context:
+        if is_calc_final:
+            prompt = "[指示]の内容を[参考情報]を元に計算してください。\n[指示]" + prompt + "\n\n[参考情報]" + context
+        else:
+            with open(config.base_path / 'prompts' / 'extract_target_money.txt', 'r') as f:
+                prompt_with_context = f.read()
+            prompt = prompt_with_context.format(context=context, user_question=prompt)
+
     chat_completion = client.chat.completions.create(
         messages=[
             {
                 "role": "user",
-                "content": prompt,
+                "content": prompt + "\n　上記の回答を、以下の内容を踏まえて回答してください。\n\n" + context if context else prompt,
             }
         ],
         model=config.openai_model,
--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
 # *-* coding: utf-8 *-*
 from llm_agent_work_utils import get_config, call_llm
-from openai import OpenAI
 from collections import namedtuple
+from pathlib import Path
 
 
 class MemoryModule:
@@ -29,7 +29,7 @@ def decompose_question(question: str, config: namedtuple):
     """
     質問を分解する
     """
-    with open('prompts/decompose.txt', 'r') as f:
+    with open(config.base_path / 'prompts' / 'decompose.txt', 'r') as f:
         decompose_prompt = f.read()
     decompose_prompt = decompose_prompt.format(user_question=question)
 
@@ -45,14 +45,85 @@ def decompose_question(question: str, config: namedtuple):
         return []
 
 
+def extract_company_name(question: str, config: namedtuple):
+    """
+    会社名を抽出する
+    """
+    with open(config.base_path / 'prompts' / 'extract_company_name.txt', 'r') as f:
+        extract_company_name_prompt = f.read()
+    extract_company_name_prompt = extract_company_name_prompt.format(user_question=question)
+
+    chat_completion = call_llm(prompt=extract_company_name_prompt, config=config)
+
+    try:
+        # JSON文字列を辞書に変換し、サブクエスチョンを取り出す
+        response_data = chat_completion.choices[0].message.content.strip()
+        company_name = eval(response_data)["company_name"]
+        if len(company_name) != 1:
+            print('会社名が複数あります。一つの会社名のみを指定してください')
+            exit(1)
+        return company_name[0]
+    except:
+        # エラーが発生した場合は空のリストを返す
+        return []
+
+
+def extract_target_period(question: str, config: namedtuple):
+    """
+    会社名を抽出する
+    """
+    with open(config.base_path / 'prompts' / 'extract_target_period.txt', 'r') as f:
+        extract_target_period_prompt = f.read()
+    extract_target_period_prompt = extract_target_period_prompt.format(user_question=question)
+
+    chat_completion = call_llm(prompt=extract_target_period_prompt, config=config)
+
+    try:
+        # JSON文字列を辞書に変換し、サブクエスチョンを取り出す
+        response_data = chat_completion.choices[0].message.content.strip()
+        year = eval(response_data)["year"]
+        quarter = eval(response_data)["quarter"]
+        return year, quarter
+    except:
+        # エラーが発生した場合は空のリストを返す
+        return []
+
+
+def get_context(year: str, quarter: str, company_name: str, config: namedtuple):
+    """
+    コンテキストを取得する
+    """
+    if not year or not quarter or not company_name:
+        return ''
+    with open(config.base_path / 'data' / company_name / 'annual_financial_results' / f'{year}_{quarter.lower()}.txt', 'r') as f:
+        context = f.read()
+    return context
+
 def agent_core(question, memory, config: namedtuple):
     memory.add_question(question)
+    print(f'大元の質問: {question}')
+    # 1. 質問を分解する。
     decomposed_questions = decompose_question(question, config)
+    print(f'分解した質問: {decomposed_questions}')
+    # 2. 会社名を取得する。
+    company_name = extract_company_name(question, config)
     answers = []
     for decomposed_question in decomposed_questions:
-        answer = call_llm(prompt=decomposed_question, config=config)
+        print(f'対象の質問:{decomposed_question}')
+        is_calc_final = False
+        # 3. 質問の中から、対象期間を抽出する。
+        year, quarter = extract_target_period(decomposed_question, config)
+        if year or quarter:
+            print(f'質問内の対象期間: {year}年{quarter}')
+        # 4. 会社名、対象期間を用いて、コンテキストを取得する。
+        context = get_context(year, quarter, company_name, config)
+        if not context:
+            context = ' '.join(memory.answer_trace)
+            is_calc_final = True
+        answer = call_llm(prompt=decomposed_question, config=config, context=context, is_calc_final=is_calc_final)
         answer = answer.choices[0].message.content
         answers.append(answer)
+        print(f'回答: {answer}')
         memory.add_answer(answer)
     # すべての回答を統合して最終回答を生成します
     final_answer = " + ".join(answers)
@@ -72,8 +143,9 @@ def main():
     # decomposeの動作確認
     # test_decompose()
 
-    config_file = 'config/config.yaml'
-    config = get_config(config_file)
+    base_path = Path(__file__).parent.parent
+    config_file = base_path / 'config' / 'config.yaml'
+    config = get_config(config_file, base_path)
 
     # メモリモジュールのインスタンス化
     memory = MemoryModule()
